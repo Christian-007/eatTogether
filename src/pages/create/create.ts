e42951ce774c0088c3ed14e28acfe141c944386e
Christian Ing <cisunardi1@sheffield.ac.uk>
2017-06-21 17:14:19 +0100

    Modified style of Create Event page

--- src/pages/create/create.ts
+++ src/pages/create/create.ts
@@ -13,16 +13,33 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'create.html',
 })
 export class CreatePage {
+  title: string; description: string;
+  loc: string; type: string;
+  startdate: any; enddate: any;
+  starttime: any; endtime: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CreatePage');
+    // this.startdate = new Date().toISOString();
+    // this.enddate = new Date().toISOString();
   }
 
   dismissModal() {
     this.navCtrl.pop();
+  }
+
+  createEvent() {
+    console.log(this.title);
+    console.log(this.description);
+    console.log(this.loc);
+    console.log(this.type);
+    console.log(this.startdate);
+    console.log(this.enddate);
+    console.log(this.starttime);
+    console.log(this.endtime);
   }
 
 }
