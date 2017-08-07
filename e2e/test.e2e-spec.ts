64f54a056c5018aa928f8fb3330d6a8d965aa4ab
Christian Ing <cisunardi1@sheffield.ac.uk>
2017-08-07 16:37:25 +0100

    Added 2 test cases for logging in

--- e2e/test.e2e-spec.ts
+++ e2e/test.e2e-spec.ts
@@ -1,33 +1,53 @@
 import { browser, element, by, ElementFinder } from 'protractor';
  
-describe('Example E2E Test', () => {
+describe('E2E Application Test', () => {
  
   beforeEach(() => {
     browser.get('');
+
+    browser.driver.sleep(500);
   });
  
-  it('the home tab is displayed by default', () => {
+  it('the login page is displayed by default', () => {
  
-      expect(element(by.css('h1')) // Grab the title of the selected tab
-        .getAttribute('innerHTML')) // Get the text content
-        .toContain("Let's Eat Together"); // Check if it contains the text "Home"
- 
+    expect(element(by.css('h1')) // Grab the title of the selected tab
+      .getAttribute('innerHTML')) // Get the text content
+      .toContain("Let's Eat Together"); // Check if it contains the text "Home"
+   
   });
- 
-  // it('the user can browse to the contact tab and view the ionic twitter handle', () => {
- 
-    // Click the 'About' tab
-    // element(by.css('[aria-controls=tabpanel-t0-2]')).click().then(() => { 
- 
-    //   // Wait for the page transition
-    //   browser.driver.sleep(1000);
- 
-    //   expect(element(by.css('ion-list ion-item ion-label')) // Grab the label of the list item
-    //     .getAttribute('innerHTML')) // Get the text content
-    //     .toContain('@ionicframework'); // Check if it contains the text "@ionicframework"
- 
-    // });
- 
+
+  // Click the 'About' tab
+  // element(by.css('[aria-controls=tabpanel-t0-2]')).click().then(() => { 
+
+  //   // Wait for the page transition
+  //   browser.driver.sleep(1000);
+
+  //   expect(element(by.css('ion-list ion-item ion-label')) // Grab the label of the list item
+  //     .getAttribute('innerHTML')) // Get the text content
+  //     .toContain('@ionicframework'); // Check if it contains the text "@ionicframework"
+
   // });
+
+  it('the user cannot login to the application', () => {
+    var alert = element(by.css('.alert-wrapper .alert-title'));
+    element(by.css('.loginButton')).click().then(() => { 
+      expect(element(by.css('.alert-wrapper .alert-title'))
+      .getAttribute('innerHTML'))
+      .toContain('Login Error'); 
+    });
+  
+  });
+
+  it('the user can login to the application', () => {
+    
+    let email: ElementFinder = element(by.css('input[name=email]'));
+    let password: ElementFinder = element(by.css('input[name=password]'));
+    email.sendKeys('b.wilson@gmail.com');
+    password.sendKeys('123');
+    element(by.css('.loginButton')).click();
+    browser.driver.sleep(1000);
+    expect(element(by.css('.sectionTitle')).getAttribute('innerHTML')).toContain('UPCOMING EVENT'); 
+
+  });
  
 });
